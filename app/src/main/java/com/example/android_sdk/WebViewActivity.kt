26a841221de30cd0adcc31048c670cc8f7234bea
Chetan Mittal <chetanm.it.22@nitj.ac.in>
2025-02-20 09:57:06 +0530

    Initial commit: Android WebView SDK implementation

---
+++ app/src/main/java/com/example/android_sdk/WebViewActivity.kt
@@ -0,0 +1,18 @@
+package com.example.android_sdk
+
+import android.os.Bundle
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AppCompatActivity
+
+class WebViewActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_web_view)
+
+        val webView = findViewById<WebView>(R.id.webView)
+        webView.settings.javaScriptEnabled = true
+        webView.webViewClient = WebViewClient()
+        webView.loadUrl("https://xaults.com")
+    }
+}
